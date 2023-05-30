a3d5b8164f8cd5580738522f50533c62e9f81a8e
prafullpathe <prafullpathe786@gmail.com>
2023-05-30 12:43:40 +0530

    change

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -5,7 +5,7 @@ function Type() {
   return (
     <Typewriter
       options={{
-        strings: ["I’m not a great programmer; I’m just a good programmer with great habits...","You can connect with me there..."],
+        strings: ["Code today, innovate tomorrow."],
         autoStart: true,
         loop: true,
         deleteSpeed: 60,
